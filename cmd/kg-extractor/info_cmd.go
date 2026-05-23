0ae74f51ae703e9bc55966e66addc992776a4aa4
norimori <norimori2@proton.me>
2026-05-23 23:33:23 +0200

    feat(extractor): add list and info subcommands

--- cmd/kg-extractor/info_cmd.go
+++ cmd/kg-extractor/info_cmd.go
@@ -3,5 +3,19 @@ package main
 import "github.com/spf13/cobra"
 
 func newInfoCmdReal(c *cliCtx) *cobra.Command {
-	return &cobra.Command{Use: "info <name>", Short: "Show plugin info", RunE: func(*cobra.Command, []string) error { return nil }}
+	return &cobra.Command{
+		Use:   "info <name>",
+		Args:  cobra.ExactArgs(1),
+		Short: "Show plugin info",
+		RunE: func(_ *cobra.Command, args []string) error {
+			plugins, _ := discoverPlugins(c.pluginsPath)
+			for _, p := range plugins {
+				if p.Manifest.Name == args[0] {
+					return writeOK(c.stdout, p.Manifest)
+				}
+			}
+			writeErr(c.stdout, "PLUGIN_NOT_FOUND", "plugin "+args[0]+" not found", "run `kg-extractor list`")
+			return errEnvelopeAlreadyWritten
+		},
+	}
 }
